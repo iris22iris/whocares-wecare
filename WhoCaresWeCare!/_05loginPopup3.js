09a2c350ac09944d0257cc942df6426df26e5398
malaaten <sa60328731@gmail.com>
2021-09-04 02:56:45 +0800

    增加popup共用檔案js

---
+++ WhoCaresWeCare!/_05loginPopup3.js
@@ -0,0 +1,65 @@
+
+document.getElementById("content").innerHTML = 
+''+
+'<div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">'+
+'        <div class="modal-dialog col-12">'+
+'                <div id="login" class="modal-content">'+
+'                    <h2 class="modal-title1 p-3 col-12 d-flex justify-content-evenly " id="exampleModalLabel">登入會員</h2>'+
+'                        <div class="containerPOPUP p-3  ">'+
+'                            <form class="row g-3 form">'+
+'                                <div class="col-3 justify-content-evenly p-1  d-flex align-items-center ">'+
+'                                    <label for="inputAccount" class="form-label">會員帳號 :</label>'+
+'                                </div>'+
+'                                <div class="col-9">'+
+'                                    <input type="text" class="form-control  " id="inputAccount">'+
+'                                </div>'+
+'                                <div class="col-3 justify-content-evenly p-1  d-flex align-items-center">'+
+'                                    <label for="inputAccount" class="form-label">會員密碼 :</label>'+
+'                                </div>'+
+'                                <div class="col-9">'+
+'                                    <input type="text" class="form-control" id="inputAccount">'+
+'                                </div>'+
+'                                <div class="form__group  ">'+
+'                                    <label>'+
+'                                        <input style="margin-left:120px;" type="checkbox" name="remember" > 記住帳密  '+
+'                                    </label>'+
+'                                </div>'+
+'                                <div class="col-3">'+
+'                                    <label for="inputAccount" class="form-label justify-content-evenly p-1  d-flex align-items-center">圖形驗證碼:</label>'+
+'                                </div>'+
+'                                <div class="col-6">'+
+'                                    <input type="text" class="form-control" id="inputAccount">'+
+'                                </div>'+
+'                                <div class="input-group  " id="box2">'+
+'                                    <span><canvas id="canvas" width="180" height="65"></canvas>'+
+'                                        <a href="#" id="changeImg">換一張</a></span>'+
+'                                </div>'+
+'                                <div class="modal-footer d-flex justify-content-center align-items-center ">                                   '+
+'                                    <div class=" ">                       '+
+'                                        <div class="button   d-flex justify-content-center align-items-center ">'+
+'                                            <input type="button" class="btn btn-primary justify-content-center " value="Login"'+
+'                                            onClick="timedMsg(this)">'+
+'                                            <button type="button" style="margin-left:50px;"  class=" btn btn-secondary d-flex justify-content-center align-items-center" data-bs-dismiss="modal">Close</button>'+
+'                                        </div>'+
+'                                        <nav class="hyperlink col-12 d-flex justify-content-center ">'+
+'                                            <div class="Hyperlinkbutton    ">'+
+'                                                <a href="" class="sideBtn">尚未註冊</a>'+
+'                                                <a href="" class="sideBtn">忘記密碼</a>'+
+'                                            </div>'+
+'                                        </nav>'+
+'                                    </div>'+
+'                                </div>'+
+'                            </form>'+
+'                        </div>'+
+'                </div>'+
+'        </div>'+
+'    </div>'
+;
+
+
+
+
+                                    
+                                
+                                        
+                                    
